ec045e69315d62c08494b09f940817ae0fef5ab9
westleaf <joakim.westblad@gmail.com>
2025-07-31 13:40:16 +0200

    player, movement and groups

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
@@ -11,7 +21,18 @@ def main():
                 return
         
         pygame.Surface.fill(screen, (0,0,0))
+
+        for thing in updateable:
+            thing.update(dt)
+
+        for thing in drawable:
+            thing.draw(screen)
+
+        # player.draw(screen)
+        # player.update(dt)
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
